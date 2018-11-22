5b579c4b2c3779648273c74e51396bb31b7b6dcb
Dmitry Lebedko <dmitrii.lebedko@t-systems.ru>
2018-11-22 14:04:56 +0100

    Configuration to provide local date in a proper format. JSR 310 Jackson implementation. Maven compiler plugin for Java 1.8.

---
+++ src/main/java/com/tsystems/tshop/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.tsystems.tshop.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.forEach((HttpMessageConverter<?> converter) -> {
+            if (converter instanceof MappingJackson2HttpMessageConverter) {
+                final MappingJackson2HttpMessageConverter jsonMessageConverter = (MappingJackson2HttpMessageConverter) converter;
+                final ObjectMapper objectMapper = jsonMessageConverter.getObjectMapper();
+                objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+            }
+        });
+        super.configureMessageConverters(converters);
+    }
+
+}
